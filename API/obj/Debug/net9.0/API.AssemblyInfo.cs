04eec6c9617b4ea3f67bfc29fa2f384d249a1d97
BerkaySimsek <berkayysimsekk@gmail.com>
2025-05-13 04:03:19 +0300

    createAsyncThunk commit

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f5ebcc8a54d35d82e8afee02b802d1d940407b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4ecffadecb944ed88f0c0c5759e67220e4126e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
