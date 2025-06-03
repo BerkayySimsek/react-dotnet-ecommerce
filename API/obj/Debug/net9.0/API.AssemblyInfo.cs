a4a02cedffee9bf05aeb412102453c5583dad34c
BerkaySimsek <berkayysimsekk@gmail.com>
2025-06-03 16:29:10 +0300

    MenuList

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19c4153582ec144193d63335d130d97fc4f92bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65921df634f5a204a320c1f92cb615197b172933")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
